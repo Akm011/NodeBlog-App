30b24182145234f2f722c80e30d258b9c3f55ab8
ASHUTOSH KUMAR MAURYA <akmashh62@gmail.com>
2024-05-25 23:24:19 +0530

    file upload and save in public directory

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const expressEdge = require('express-edge')
 const bodyParser = require("body-parser");
 const connectToMongoose = require("./database")
-// const fileUpload = require("express-fileupload");
+const fileUpload = require("express-fileupload");
+const path = require('path');
 
-const Post= require('./database/models/Post')
+
+const Post = require('./database/models/Post')
 
 connectToMongoose();
 const app = new express()
 
+app.use(fileUpload())
 app.use(express.static('public'))  //use public directory from here
 app.use(expressEdge)  //.use add functionality to express
 app.set('views', `${__dirname}/views`)
@@ -47,15 +50,46 @@ app.get('/posts/new', (req, res) => {
     res.render('create')
 })
 
+
 app.post('/posts/store', async (req, res) => {
-    try {
-        await Post.create(req.body);
-        res.redirect('/');
-    } catch (error) {
-        console.error(error);
-        res.redirect('/');
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
     }
+
+    const { image } = req.files;
+    const imagePath = path.resolve(__dirname, 'public/posts', image.name);
+
+    image.mv(imagePath, async (error) => {
+        if (error) {
+            console.error(error);
+            return res.status(500).send(error);
+        }
+
+        try {
+            await Post.create({
+                ...req.body,
+                image: `/posts/${image.name}` // Store the path to the image in the database
+            });
+            res.redirect('/');
+        } catch (error) {
+            console.error(error);
+            res.redirect('/');
+        }
+    });
 });
+// app.post('/posts/store', async (req, res) => {
+//     console.log(req.files);
+//     const { image } = req.files
+//     image.mv(path.resolve(__dirname, 'public/posts', image.name), (error) => {
+//         try {
+//             await Post.create(req.body);
+//             res.redirect('/');
+//         } catch (error) {
+//             console.error(error);
+//             res.redirect('/');
+//         }
+//     })
+// });
 
 app.listen(4000, () => {
     console.log("App listening on port 4000")
